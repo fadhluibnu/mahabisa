8e4288f5416d321e9225fd2d05d5b516bfe39ab9
fadhluibnu <f.ibnu.a1@gmail.com>
2025-05-20 17:47:14 +0700

    install prettier

--- resources/js/Pages/Test/Test.jsx
+++ resources/js/Pages/Test/Test.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Test = () => {
-    return (
-        <div>
-            <h1 class="text-3xl font-bold underline">Test Page</h1>
-            <p>This is a test page.</p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1 class='text-3xl font-bold underline'>Test Page</h1>
+      <p>This is a test page.</p>
+    </div>
+  );
+};
 
-export default Test
+export default Test;
--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-        react()
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+    react(),
+  ],
 });
